53f687dd24ccbe8c173d3f494b36b7506c302071
Raffaele Spanò <nemoraffa@gmail.com>
2024-08-17 18:12:05 +0200

    feat: enable transform controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,16 @@
-import { OrbitControls } from '@react-three/drei'
+import { TransformControls, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
 
 export default function Experience()
 {
+    const cubeRef = useRef(null);
+
     return <>
-        <OrbitControls />
+        <OrbitControls makeDefault /> 
+        {/* 
+            The prop enabledDumping is enabled by default to make the animation of the camera movement smooth
+            The prop makeDefault solves the issue regarding the TransformControls where dragging the object around caused the camera to move as well    
+        */}
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
@@ -13,10 +20,11 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={cubeRef} position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cubeRef} mode='translate' />
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
